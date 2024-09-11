babce2b17afad5a09cbadda8098978dfbd390f6c
lucasbiz <lucaasbizteixeira@gmail.com>
2024-09-10 21:41:52 -0300

    teste

--- angular/first-app_01-hello-world/src/app/app.component.ts
+++ angular/first-app_01-hello-world/src/app/app.component.ts
@@ -1,20 +1,23 @@
 import {Component} from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { HousingLocationComponent } from "./housing-location/housing-location.component";
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, HousingLocationComponent],
+  imports: [HomeComponent, HousingLocationComponent, RouterModule],
   template: `
     <main>
+      <a [routerLink]="['/']">
         <header class="brand-name">
           <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
         </header>
-        <section class="content">
-          <app-home></app-home> <!-- <app-home> foi a tag criada e definida como seletor no home.component  -->
-        </section>
-      </main>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet> <!-- <router-outlet> foi a tag criada e definida como seletor no routermodule  -->
+      </section>
+    </main>
   `,
   styleUrls: ['./app.component.css'],
 })
@@ -24,3 +27,4 @@ export class AppComponent {
 
 
 // Angular apps are built around components, which are Angular's building blocks. Components contain the code, HTML layout, and CSS style information that provide the function and appearance of an element in the app. In Angular, components can contain other components. An app's functions and appearance can be divided and partitioned into components.'
+
